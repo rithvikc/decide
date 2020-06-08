09e135a50d3e5ca388fdbb24d884f55fe1d37ffa
Arie <arie.theodorus.vanderpoel@gmail.com>
2020-06-08 20:33:58 +1200

    added location to invitations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_08_073145) do
+ActiveRecord::Schema.define(version: 2020_06_08_083231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2020_06_08_073145) do
     t.bigint "cuisine_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "location"
     t.index ["cuisine_id"], name: "index_invitations_on_cuisine_id"
     t.index ["event_id"], name: "index_invitations_on_event_id"
     t.index ["user_id"], name: "index_invitations_on_user_id"
