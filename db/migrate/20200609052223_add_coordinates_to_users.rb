6992daca420e5778ff207faa17561d9d8beea012
Rithvik Chand <rithvikchand@gmail.com>
2020-06-09 15:26:18 +1000

    geocode complete

--- db/migrate/20200609052223_add_coordinates_to_users.rb
+++ db/migrate/20200609052223_add_coordinates_to_users.rb
@@ -2,6 +2,6 @@ class AddCoordinatesToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :latitude, :float
     add_column :users, :longitude, :float
-    add_column :address, :longitude, :float
+    add_column :users, :address, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_08_083231) do
+ActiveRecord::Schema.define(version: 2020_06_09_052223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,6 +81,9 @@ ActiveRecord::Schema.define(version: 2020_06_08_083231) do
     t.string "name"
     t.string "allergies"
     t.string "avatar"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
