0b8485e855d49888cbc2caef7f822fb83652e2a9
Rithvik Chand <rithvikchand@gmail.com>
2020-06-09 15:23:27 +1000

    geocode complete

---
+++ db/migrate/20200609052223_add_coordinates_to_users.rb
@@ -0,0 +1,7 @@
+class AddCoordinatesToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :latitude, :float
+    add_column :users, :longitude, :float
+    add_column :address, :longitude, :float
+  end
+end
