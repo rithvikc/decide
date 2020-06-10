8d766f43ddb1f41764cb6809ad317c8aa562f46d
Lacy Zhang <lacy.zhang@hotmail.com>
2020-06-10 12:03:38 +1000

    application controller page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,8 +13,8 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def after_sign_in_path_for(resource)
-    # return the path based on resource
-    '/home'
-  end
+  # def after_sign_in_path_for(resource)
+  #   # return the path based on resource
+  #   'pages/home'
+  # end
 end
