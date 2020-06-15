59f8e4ba000378bd648a79c7c6767363ba8f9702
Arie <arie.theodorus.vanderpoel@gmail.com>
2020-06-15 19:32:46 +1200

    Added invitation controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_06_15_032409) do
+ActiveRecord::Schema.define(version: 2020_06_15_065617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
