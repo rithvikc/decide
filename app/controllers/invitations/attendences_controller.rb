96aa2861e3e5751eb267252c002b711a496cf405
Lacy Zhang <lacy.zhang@hotmail.com>
2020-06-17 15:41:20 +1000

    FIX attending action cable bug fixed

--- app/controllers/invitations/attendences_controller.rb
+++ app/controllers/invitations/attendences_controller.rb
@@ -2,12 +2,23 @@ class Invitations::AttendencesController < ApplicationController
   def destroy
     @event = Event.find(params[:event_id])
     current_user.set_status_for(@event, "Cancelled")
+    event_channel
     redirect_to event_path(@event)
   end
 
   def create
     @event = Event.find(params[:event_id])
     current_user.set_status_for(@event, "Confirmed")
+    event_channel
     redirect_to event_path(@event)
+  end
+
+  private
+
+  def event_channel
+    EventChannel.broadcast_to(
+      @event,
+      render_to_string(partial: "shared/invited", locals: { event: @event })
+    )
   end
 end
