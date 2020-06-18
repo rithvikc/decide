efb7f50dde4f767eae1d15338d802e7b8f7c28ba
Arie <arie.theodorus.vanderpoel@gmail.com>
2020-06-18 23:25:25 +1200

    Action cable fix w. javascript

--- app/javascript/channels/event_channel.js
+++ app/javascript/channels/event_channel.js
@@ -1,5 +1,4 @@
 import consumer from "./consumer";
-
 const initEventCable = () => {
   console.log('eventCable');
   const invitesContainer = document.getElementById('invite-container');
@@ -7,11 +6,13 @@ const initEventCable = () => {
     const id = invitesContainer.dataset.eventId;
     consumer.subscriptions.create({ channel: "EventChannel", id: id }, {
       received(data) {
-        console.log("updated");
         invitesContainer.innerHTML = data // called when data is broadcast in the cable
+        document.body.insertAdjacentHTML('afterend', `<div class="alert alert-info alert-dismissible" role="alert">
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button>
+          Your invitation has been sent!
+        </div>`);
       },
     });
   }
 }
-
 export { initEventCable };
