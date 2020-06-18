41c66537de21bb18f07a28b37876c8f8ff57add7
Lacy Zhang <lacy.zhang@hotmail.com>
2020-06-19 00:27:00 +1000

    FIX invitations show on event page

--- app/javascript/channels/event_channel.js
+++ app/javascript/channels/event_channel.js
@@ -14,13 +14,7 @@ const initEventCable = () => {
     consumer.subscriptions.create({ channel: "EventChannel", id: id }, {
       received(data) {
         invitesContainer.innerHTML = data // called when data is broadcast in the cable
-        document.body.insertAdjacentHTML('afterend', `<div class="alert alert-info alert-dismissible" role="alert">
-          <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button>
-          Your invitation has been sent!
-        </div>`);
-        
         cleanInputBox();
-
       },
     });
   }
